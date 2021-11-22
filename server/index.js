70c1c3f7a35cde98626cfd9423e54004c6bf4eb5
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-22 18:39:44 +0300

    Update server index.js and routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
-
-
+const authRouter = require("./routes/auth.routes")
 const app = express()
 const PORT = config.get('ServerPort')
 
+app.use(express.json())
+app.use("/api/auth", authRouter)
 
 const start = () => {
     try {
