8c5dbaf6f43eef6bb44276d7311ba6c3b22b7d12
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-23 13:17:35 +0300

    edit db model

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ const { Schema, model } = require("mongoose")
 const User = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
+    username: { type: String, required: true },
     listsId: { type: Array, required: false }
 })
 
