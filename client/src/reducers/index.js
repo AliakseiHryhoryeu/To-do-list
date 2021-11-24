e5fcebac6aab23e712b81f990165f3304132db20
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-24 11:25:47 +0300

    Add userReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
+import useReducer from './userReducer';
 
 const rootReducer = combineReducers({
-
+    user:useReducer,
 })
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,5 +1,17 @@
 
+const defaultState = {
+    curentUser:{},
+    isAuth: false
+}
 
+
+export default function useReducer(state = defaultState, action){
+    switch(action.type){
+        
+        default:
+            return state
+    }
+}
 
 
 
