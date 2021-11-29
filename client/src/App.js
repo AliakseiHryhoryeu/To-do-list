d9bfe5511d501a9a127fc16d5a72a90849386239
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-29 13:47:38 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
+import Page404 from "./components/Page404";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div className="todo">
       <Header />
+      <Page404 />
+      <Footer />
     </div>
   );
 }
