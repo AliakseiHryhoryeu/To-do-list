c3a8751d9ff8073c948827aef8e787fb1264cd40
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-29 14:35:21 +0300

    add page404

---
+++ client/src/pages/Page404/index.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+import Header from '../../components/Header'
+
+export default function Page404() {
+    return (
+        <Fragment >
+            <div className="Page404">
+                <Header />
+                page 404
+            </div>
+        </Fragment>
+
+    )
+}
