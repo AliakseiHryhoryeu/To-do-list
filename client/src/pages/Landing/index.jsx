a01ac4a1ec6ef5475cada7008fe1938dfcc15aed
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-29 14:35:31 +0300

    add landing page

---
+++ client/src/pages/Landing/index.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+import Header from '../../components/Header'
+
+export default function Landing() {
+    return (
+        <Fragment>
+            <div className="Landing">
+                <Header />
+                Landing
+            </div>
+        </Fragment>
+
+    )
+}
