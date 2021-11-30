af7935b240170b10298ac81581b95f4db688c486
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-30 11:21:32 +0300

    upd slider

--- client/src/components/Slider/index.jsx
+++ client/src/components/Slider/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import './Slider.scss'
+
 export default function Slider() {
     return (
-        <div>
-            
+        <div className="slider">
+            <div className="slider__buttons">
+                <button className="slider__buttons-prev">Prev</button>
+            </div>
+            <div className="slider__container">
+                <div className="slider__track">
+                    <div className="slider__item"></div>
+                    <div className="slider__item"></div>
+                    <div className="slider__item"></div>
+                    <div className="slider__item"></div>
+                </div>
+            </div>
+            <div className="slider__buttons">
+                <button className="slider__buttons-prev">Next</button>
+            </div>
         </div>
     )
 }
