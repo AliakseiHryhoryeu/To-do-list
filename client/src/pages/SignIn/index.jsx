eeb7517c798810302633888f1fae4962db4abba3
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-24 20:52:07 +0300

    add sign in page

---
+++ client/src/pages/SignIn/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Header from '@components/Header'
+
+export default function SignIn() {
+    return (
+        <div>
+            <Header />
+            signIn
+        </div>
+    )
+}
