546e1fd979d19257136dc8c7d5cc380d701f4292
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-29 09:00:09 +0300

    Update Task.js

--- server/models/Task.js
+++ server/models/Task.js
@@ -2,12 +2,10 @@ const { Schema, model } = require("mongoose")
 
 
 const Task = new Schema({
-    id: { type: Number, required: true },
     ListId: { type: String, required: true },
     text: { type: String, required: true },
     completed: { type: Array, required: true, default: false },
 })
-
 
 
 module.exports = model('Task', Task)
