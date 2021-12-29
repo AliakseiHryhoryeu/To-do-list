c284c7450e4b7cb1b371cafd8c1dd83e3d343939
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-29 09:35:02 +0300

    Update index.jsx

--- client/src/pages/Registration/index.jsx
+++ client/src/pages/Registration/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+
+
+import { useFormik } from 'formik';
+import { registerSchema } from './validation';
 
 import Header from '@components/Header'
 
 import './Registration.scss'
 
 export default function Registration() {
+    const formik = useFormik({
+        initialValues: {
+          username: '',
+          email: '',
+          password: ''
+        },
+        validationSchema: registerSchema,
+        onSubmit: (values) => {
+          registerUserWithEmail(values, history);
+        },
+      });
+
     return (
         <div className='registration'>
             <Header />
@@ -12,13 +29,18 @@ export default function Registration() {
                 <div className="registration__form">
                     <div className="registration__title">Registration</div>
 
-                    <input className="registration__input" type="text" placeholder='User name...' />
+                    <input className="registration__input" 
+                        type="text" placeholder='User name...' 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.name}
+                    />
                     <input className="registration__input" type="text" placeholder='Email Adress...' />
-                    <input className="registration__input" type="text" placeholder='Password...' />
-                    <input className="registration__input" type="text" placeholder='Repeat your password...' />
+                    <input className="registration__input" type="password" placeholder='Password...' />
+                    <input className="registration__input" type="password" placeholder='Repeat your password...' />
                     
-                    <a href="" className="registration__button-reg">Register</a>
-                    <a href="" className="registration__button-signIn">Alredy have account? Sign in</a>
+                    <button href="" type="submit" className="registration__button-reg">Register</button>
+                    <Link to="/auth" className="registration__button-signIn">Alredy have account? Sign in</Link>
                 </div>
             </div>
         </div>
