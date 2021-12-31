d85f5c66f9edda66f509e7672a53300e60210560
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-31 10:23:11 +0300

    Update userReducer.js

--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -1,15 +1,33 @@
+import axios from "axios"
+import {
+    SET_USER,
+    LOGOUT,
+  } from '@store/types';
 
 const defaultState = {
     curentUser:{},
     isAuth: false
 }
 
-
-export default function useReducer(state = defaultState, action){
-    switch(action.type){
-        
+export default function userReducer(state = defaultState, action) {
+    switch (action.type) {
+        case SET_USER:
+            return {
+                ...state,
+                currentUser: action.payload,
+                isAuth: true
+            }
+        case LOGOUT:
+            localStorage.removeItem('token')
+            return {
+                ...state,
+                currentUser: {},
+                isAuth: false
+            }
         default:
             return state
     }
 }
 
+export const setUser = user => ({type: SET_USER, payload: user})
+export const logout = () => ({type: LOGOUT})
