662b9df76525b2f1144823fd1f13cfcfc32e981a
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-01-11 20:11:47 +0300

    add settings menu in header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -8,6 +8,8 @@ import classNames from 'classnames';
 
 import mainLogo from '@img/favicon.svg'
 import userIcon from '@img/userIcon.png'
+import settingsIcon from '@img/settingsIcon.svg'
+import exitIcon from '@img/exitIcon.svg'
 
 import './Header.scss';
 
@@ -22,15 +24,19 @@ const Header = ({ }) => {
 
   // responsive at mobile devices
   const [isActiveHeaderBurger, setActiveHeaderBurger] = useState(false)
-  const toggleClassActive = () => {
+  const [isActiveUsername, setActiveUsername] = useState(false)
+  const toggleClassActiveHeaderBurger = () => {
     setActiveHeaderBurger(!isActiveHeaderBurger)
+  }
+  const toggleClassActiveUsername = () => {
+    setActiveUsername(!isActiveUsername)
   }
   return (
     <header className="header__wrapper">
       <header className="header">
         <div className="header__container">
 
-          <div className={classNames("header__burger", { 'active': isActiveHeaderBurger })} onClick={toggleClassActive}>
+          <div className={classNames("header__burger", { 'header__burger-active': isActiveHeaderBurger })} onClick={toggleClassActiveHeaderBurger}>
             <span></span>
           </div>
           <Link to="/" className="header__link">
@@ -38,7 +44,7 @@ const Header = ({ }) => {
             To do list
           </Link>
 
-          {!isAuth && <nav className={classNames("header__nav", { 'active': isActiveHeaderBurger })}>
+          {!isAuth && <nav className={classNames("header__nav", { 'header__nav-active': isActiveHeaderBurger })}>
             <ul className="header__nav__list">
               <li className="nav__item">
                 <Link to="/registration" className="header__nav__link">Registration</Link>
@@ -49,24 +55,29 @@ const Header = ({ }) => {
             </ul>
           </nav>}
 
-          {isAuth && <nav className={classNames("header__nav", { 'active': isActiveHeaderBurger })}>
-            <div className="header__nav__list">
-              <div className="header__nav__item header__nav__item-username">
-                <div className="header__nav__username">
-                  <div to="/" className="header__nav__link">Username</div>
+          {isAuth && <nav className={classNames("header__nav", { 'header__nav-active': isActiveHeaderBurger })}>
+            <ul className="header__nav__list">
+              <li className="header__nav__item header__nav__username">
+
+                <div className="header__nav__username__container" onClick={toggleClassActiveUsername}>
+                  <div className="header__nav__link">Username</div>
                   <img src={userIcon} alt="userIcon" />
                 </div>
-                <div className="header__nav__username-content">
-                  <div className="header__nav__item" onClick={() => dispatch(logout())}>
-                    <Link to="/" className="header__nav__link" >Logout</Link>
-                  </div>
-                  <div className="header__nav__item">
+
+
+                <div className={classNames("header__nav__username__content", { 'header__nav__username__content-active': isActiveUsername })}>
+                  <div className="header__nav__username__item">
+                    <img src={settingsIcon} alt="settingsIcon" />
                     <a href="/" className="header__nav__link" >Settings</a>
                   </div>
+                  <div className="header__nav__username__item" onClick={() => dispatch(logout())}>
+                    <img src={exitIcon} alt="exitIcon" />
+                    <Link to="/" className="header__nav__link" >Logout</Link>
+                  </div>
                 </div>
+              </li>
 
-              </div>
-            </div>
+            </ul>
           </nav>}
 
 
