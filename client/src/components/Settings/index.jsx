13db4caf85ead9e1c4768df3cd2667f212fa800c
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-03-21 16:26:35 +0300

    upd user images

--- client/src/components/Settings/index.jsx
+++ client/src/components/Settings/index.jsx
@@ -7,17 +7,10 @@ import { hideSettings } from '@actions/userActions'
 
 import './Settings.scss'
 
-import img1 from '@img/userIcon_1.png'
-import img2 from '@img/userIcon_2.png'
-import img3 from '@img/userIcon_3.png'
-import img4 from '@img/userIcon_4.png'
-import img5 from '@img/userIcon_5.png'
+
 
 import closebtn from '@img/remove.svg'
 import UserIcons from './UserIcons/';
-
-const allUserIcons = [img1, img2, img3, img4, img5]
-
 
 function Settings(props) {
 
--- client/src/components/Settings/userIcons/index.jsx
+++ client/src/components/Settings/userIcons/index.jsx
@@ -4,6 +4,13 @@ import { useDispatch, connect } from 'react-redux';
 
 import { setActiveUserIcon } from '@actions/userActions'
 
+import img1 from '@img/userIcon_1.png'
+import img2 from '@img/userIcon_2.png'
+import img3 from '@img/userIcon_3.png'
+import img4 from '@img/userIcon_4.png'
+import img5 from '@img/userIcon_5.png'
+const allUserIcons = [img1, img2, img3, img4, img5]
+
 
 function userIcons(props) {
 
@@ -12,10 +19,10 @@ function userIcons(props) {
   console.log(props.image)
   try {
     return (
-      props.images.map((image, index) =>
+      allUserIcons.map((image, index) =>
         <img className={classNames("settings__usericon-img", { "settings__usericon-active": (props.image === image) })}
           key={index} src={image} alt="image"
-          onClick={() => dispatch(setActiveUserIcon(image))}
+          onClick={() => dispatch()}
         />)
     )
 
